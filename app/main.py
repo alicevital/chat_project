0ea65fcd971c55980591e44412ca8923a24130ff
Thiago Pratti de Mendonça <thiago.mendonca@bsatech.io>
2025-11-04 16:55:55 -0300

    feat: adicionando front end ao projeto

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from app.controllers.user_controller import UserRouter
+from fastapi.responses import FileResponse
 
 # Criação da API
 app = FastAPI(title="Chat API")
 
 # Rotas da API
 app.include_router(UserRouter)
+
+
+# Front-End
+@app.get("/")
+def root():
+    return FileResponse("app/views/html/cadastro.html")

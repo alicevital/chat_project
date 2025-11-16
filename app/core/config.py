c2062c576e1f34f4043fef98919f1d7c2eecd334
Alice Emily <alice.nascimento@bsatech.io>
2025-11-16 18:57:34 -0300

    feat: adiciona configurações do mongodb

--- app/core/config.py
+++ app/core/config.py
@@ -6,3 +6,5 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://root:root@localhost:27017") # no computador: mongodb://root:root@localhost:27017 em produção: mongodb+srv://root:<db_password>@cluster0.i6ffdcs.mongodb.net/
+DATABASE_NAME = "chat_database"
